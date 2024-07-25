cd5e8f3909345f55d96ba2b89b534728c5ff6fac
snm115 <snm115@scarletmail.rutgers.edu>
2024-07-24 22:44:20 -0400

    Continued working on adding file-processing argument.

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-import argparse
+import argparse, os
+from argparse import Namespace
 
 if __name__ == "__main__":
     msg = "Input a command."
 
     parser = argparse.ArgumentParser(description=msg)
-    parser.add_argument('integers', metavar = 'N', type = int, nargs = '+', help = 'an integer for the accumulator')
-    parser.add_argument('--sum', dest = 'accumulate', action = 'store_const', const = sum, default = max, 
-                        help = 'sum the integers (default: find the max)')
+    #parser.add_argument('integers', metavar = 'N', type = int, nargs = '+', help = 'an integer for the accumulator')
+    #parser.add_argument('--sum', dest = 'accumulate', action = 'store_const', const = sum, default = max, 
+    #                    help = 'sum the integers (default: find the max)')
     
-    parser.add_argument('-c', help = 'count the number of bits in a file')
-    args = parser.parse_args()
+    parser.add_argument('-c', '--countbits', help = 'count the number of bits in a file', 
+                        action = 'store_true')
+    parser.add_argument('filename',metavar = 'f', type = argparse.FileType('r'), help = 'a file name')
+    args: Namespace = parser.parse_args()
 
     print("hello")
-    print(args.accumulate(args.integers))
+    #print(args.accumulate(args.integers))
+    if args.countbits:
+        file_size = os.stat(args.filename)
+        print(file_size)
+   
     
